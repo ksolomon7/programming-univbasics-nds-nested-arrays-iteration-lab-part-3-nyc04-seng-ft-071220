95a4a7dcd18a6c3686c909b222e7bf84c44e6a33
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 15:34:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,9 +3,10 @@ def join_nested_strings(src)
   outer_count=0
   while outer_count<src.count do
     inner_count=0
+    newer_string=" "
     while inner_count<src[outer_count].count do
       if src[outer_count][inner_count].class==String
-        new_string.join(src[outer_count][inner_count])
+        newer_string.push(src[outer_count][inner_count])
       end
     inner_count+=1
     end
