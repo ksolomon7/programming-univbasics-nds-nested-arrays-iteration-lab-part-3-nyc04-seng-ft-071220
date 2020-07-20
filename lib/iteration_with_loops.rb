244ec23f630807efd22e52999730a60997bb9e10
Kalkidan Taye <kalkidan.solomon2@gmail.com>
2020-07-20 15:23:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,10 +3,10 @@ def join_nested_strings(src)
   outer_count=0
   while outer_count<src.count do
     inner_count=0
-
+    string_joining=" "
     while inner_count<src[outer_count].count do
       if src[outer_count][inner_count]
-        new_string.push(src[outer_count][inner_count].join(" "))
+        new_string.join(src[outer_count][inner_count])
     end
     inner_count+=1
   end
